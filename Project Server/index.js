2c38b2c76b3f1ba00e17257558d14f4cf59f689e
hamzakhalil3343 <hamza.khalil334@gmail.com>
2019-12-26 04:16:40 +0500

    backend of admin and b/f end  of user

---
+++ Project Server/index.js
@@ -0,0 +1,183 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/carRentalDb");
+mongoose.connection.on('open', () => {
+  console.log('Connected to mongodb server.');
+  mongoose.connection.db.listCollections().toArray(function (err, names) {
+    console.log(names);
+   });
+})
+
+//this maintain our req.body 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+//creating model of user
+const Users = mongoose.model('users', {
+    name: String,
+    user_id: Number,
+    email: String,
+    password: String,
+    date_added: Date
+   });
+
+//creating model for admin
+const Admins = mongoose.model('admins', {
+  name: String,
+  admin_id: Number,
+  email: String,
+  password: String,
+  date_added: Date
+ });
+
+
+
+//signup user func
+app.post('/signup', async (req, res) => {
+    const body = req.body;
+    console.log('req.body', body)
+      try{
+    const User = new Users(body);
+    
+    const result = await User.save();
+    res.send({
+      message: 'user signup successful'
+    });
+    
+      }
+      catch(ex){
+        console.log('ex',ex);
+        res.send({message: 'Error'}).status(401);
+      };
+
+    });
+
+
+
+    //user login function
+
+    app.post('/login',  async (req, res) => {
+      const body = req.body;
+      console.log('req.body', body);
+  
+      const email = body.email;
+  
+      // lets check if email exists
+  
+      const result = await Users.findOne({"email":  email});
+      if(!result) // this means result is null
+      {
+        res.status(401).send({
+          Error: 'This user doesnot exists. Please signup first'
+         });
+      }
+      else{
+        // email did exist
+        // so lets match password
+  
+        if(body.password === result.password){
+  
+          // great, allow this user access
+  
+          console.log('match');
+          
+  
+          res.send({message: 'Successfully Logged in'});
+        }
+  
+          else{
+  
+            console.log('password doesnot match');
+  
+            res.status(401).send({message: 'Wrong email or Password'});
+          }
+  
+  
+      }
+  
+    });     
+
+//admin sign up function
+app.post('/signupadmin', async (req, res) => {
+  const body = req.body;
+  console.log('req.body', body)
+    try{
+  const admin = new Admins(body);
+  
+  const result = await admin.save();
+  res.send({
+    message: 'Admin signup successful'
+  });
+  
+    }
+    catch(ex){
+      console.log('ex',ex);
+      res.send({message: 'Error'}).status(401);
+    };
+
+  });
+
+
+     //admin Login sucsessFull
+
+     app.post('/loginAdmin',  async (req, res) => {
+      const body = req.body;
+      console.log('req.body', body);
+  
+      const email = body.email;
+  
+      // lets check if email exists
+  
+      const result = await Admins.findOne({"email":  email});
+      if(!result) // this means result is null
+      {
+        res.status(401).send({
+          Error: 'This user doesnot exists. Please signup first'
+         });
+      }
+      else{
+        // email did exist
+        // so lets match password
+  
+        if(body.password === result.password){
+  
+          // great, allow this user access
+  
+          console.log('match');
+          
+  
+          res.send({message: 'Admin Successfully Logged in'});
+        }
+  
+          else{
+  
+            console.log('password doesnot match');
+  
+            res.status(401).send({message: 'Wrong email or Password'});
+          }
+  
+  
+      }
+  
+    });     
+
+
+
+    //app.get /
+app.get('/', (req, res) => { 
+    res.send('hello world');
+  });
+
+
+  app.get('/Users', (req, res) => { 
+    res.send('Hello Users');
+  });
+
+app.listen(3000, () => {
+  console.log('Express application running on localhost:3000');
+});
